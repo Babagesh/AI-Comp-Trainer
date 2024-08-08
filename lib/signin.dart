5ba89234fb0e5afa7e1d9ba9b00a953fac3fe291
Bhagyesh Jethwani <jethwani.bhagyesh@gmail.com>
2024-08-08 17:14:54 -0400

    login ui setup

--- lib/signin.dart
+++ lib/signin.dart
@@ -9,6 +9,8 @@ class SignInScreen extends StatefulWidget{
 
 class _SignInScreenState extends State<SignInScreen>
 {
+  TextEditingController _passwordTextController = TextEditingController();
+  TextEditingController _emailTextController = TextEditingController();
   @override 
   Widget build(BuildContext context)
   {
@@ -27,7 +29,19 @@ class _SignInScreenState extends State<SignInScreen>
               20, MediaQuery.of(context).size.height * 0.2, 20, 0),
             child: Column(
               children:<Widget> [
-                  logoWidget("assets/images/daniyl.png"),
+                  Image.asset("assets/images/daniyl.png"),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  reusableTextField("Enter UserName", Icons.person_outline, false, _emailTextController),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  reusableTextField("Enter Password", Icons.lock_outline, true, _passwordTextController),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  signUpButton(context, true, () {})
               ],
               ),
               ),
